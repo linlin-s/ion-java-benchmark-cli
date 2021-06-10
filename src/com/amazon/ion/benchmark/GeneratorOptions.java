85da157d59bf924331ef6848b725832b4c957c43
Sun <linls@88665a247dd3.ant.amazon.com>
2021-06-09 23:24:30 -0700

    Add the ability to generate streams of specified range/format random decimals.

---
+++ src/com/amazon/ion/benchmark/GeneratorOptions.java
@@ -0,0 +1,16 @@
+package com.amazon.ion.benchmark;
+
+import java.util.Map;
+
+public class GeneratorOptions {
+        
+    public static void excuteGenerator(Map<String, Object> optionsMap) throws Exception {
+            
+        String format = optionsMap.get("--format").toString().substring(1,optionsMap.get("--format").toString().length()-1);
+        String exp_range = optionsMap.get("--decimal-exponent-range").toString() ;
+        String coeffi_digits= optionsMap.get("--decimal-coefficient-digit-range").toString();
+        String [] x = exp_range.split(",");
+        
+        WriteRandomIonValues.writeRandomDecimals(Integer.valueOf(optionsMap.get("<data_size>").toString()), optionsMap.get("<output_file>").toString(), format, exp_range, coeffi_digits);
+    }       
+}
